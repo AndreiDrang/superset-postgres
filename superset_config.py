bf75557ea2e13095900399d293044bb7eaa548bd
Andrei <drop.all.requests@gmail.com>
2025-11-30 23:25:17 +0300

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -12,7 +12,7 @@ SQLALCHEMY_DATABASE_URI = db_uri or "sqlite:////app/superset_home/superset.db"
 # More info - https://superset.apache.org/docs/configuration/cache/
 CACHE_CONFIG: Dict[str, Any] = {
     "CACHE_TYPE": "RedisCache",
-    "CACHE_DEFAULT_TIMEOUT": 600,
+    "CACHE_DEFAULT_TIMEOUT": os.environ.get("CACHE_DEFAULT_TIMEOUT", 600),
     "CACHE_KEY_PREFIX": "superset_metadata_cache",
     "CACHE_REDIS_HOST": os.environ.get("CACHE_REDIS_HOST"),
     "CACHE_REDIS_PORT": os.environ.get("CACHE_REDIS_PORT"),
